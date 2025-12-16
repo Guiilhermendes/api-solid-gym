7fed77f6e7f473bce56855ece2f3689362579fc8
Home <home@MacBook-Air-de-Home.local>
2025-12-16 20:26:19 -0300

    feat: implementing authorization JWT

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -15,8 +15,16 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({ email, password })
+    const authenticateUseCase = makeAuthenticateUseCase();
+    const { user } = await authenticateUseCase.execute({ email, password });
+
+    const token = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id
+      }
+    });
+
+    return reply.status(200).send({token})
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
@@ -24,6 +32,4 @@ export async function authenticate(
 
     throw err
   }
-
-  return reply.status(200).send()
 }
